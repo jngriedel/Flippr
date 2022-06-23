a47aa8dbc42ceeaaa13f0fe2c12c71c07bae59ac
jngriedel <jngriedel@gmail.com>
2022-06-23 13:26:34 -0700

    navbar

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -49,7 +49,7 @@ function LoginFormPage() {
           required
         />
       </label>
-      <button type="submit">Log In</button>
+      <button className='login-bttn' type="submit">Log In</button>
     </form>
     </div>
   );
--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -69,7 +69,7 @@ function SignupFormPage() {
           required
         />
       </label>
-      <button type="submit">Sign Up</button>
+      <button className='login-bttn' type="submit">Sign Up</button>
     </form>
   );
 }
