08f5a4730a0b3629ee4da673313941bc661d5d39
jngriedel <jngriedel@gmail.com>
2022-06-23 10:06:41 -0700

    phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+
+
+// =============Authority Tests==============
+router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+      return res.json(req.user);
+    }
+);
+
+
+router.get(
+  '/restore-user',
+  restoreUser,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
+
+
 
 module.exports = router;
