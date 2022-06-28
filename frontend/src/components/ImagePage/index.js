a4c5ea34986289e8fa12dbdf73b8d123a7ea502e
jngriedel <jngriedel@gmail.com>
2022-06-27 22:35:31 -0700

    image page styling

--- frontend/src/components/ImagePage/index.js
+++ frontend/src/components/ImagePage/index.js
@@ -63,21 +63,32 @@ const editDescription = async(e) => {
       <div className='view-single-image'>
 
         {
-            <div >
+            < >
                 <img className='image-alone' src={myImage?.imageUrl}/>
+                {myImage &&
+                    <div className='delete-holder'>
+                      <button id='img-delete' onClick={()=>deleteImage(myImage.id)}
+                      style={{visibility: myImage.userId === sessionUser?.id ? "visible" : "hidden"}}><i className="fa fa-trash fa-2x" aria-hidden="true"></i></button>
 
-            </div>
+                    </div>}
+            </>
 
         }
     </div>
         <div className='single-image-details'>
             {!editContent && myImage &&
-                    <>
-                    <span>{myImage?.content}</span>
-                    <button
-                    onClick={()=>setEditContent(true)}
-                    style={{visibility: myImage.userId === sessionUser?.id ? "visible" : "hidden"}}><i className="fas fa-edit"></i></button>
-                    </>
+                    <div className='image-detail-box'>
+                        <div className='username-box'>
+                            <h3>User Placeholder</h3>
+                        </div>
+                        <div className='description-and-button'>
+                            <span>{myImage?.content}</span>
+                            <button
+                            className='comment-hidden-bttn'
+                            onClick={()=>setEditContent(true)}
+                            style={{visibility: myImage.userId === sessionUser?.id ? "visible" : "hidden"}}><i className="fas fa-edit"></i></button>
+                        </div>
+                    </div>
                         }
 
 
@@ -103,12 +114,7 @@ const editDescription = async(e) => {
 
                 </>
                 }
-                    {myImage &&
-                    <div>
-                      <button onClick={()=>deleteImage(myImage.id)}
-                      style={{visibility: myImage.userId === sessionUser?.id ? "visible" : "hidden"}}><i className="fa fa-trash" aria-hidden="true"></i></button>
 
-                    </div>}
 
 
 
